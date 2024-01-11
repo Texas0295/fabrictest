4ef0b67619361e2cb210eaecd5b5d0d52a095166
Texas0295 <Texas0295@outlook.com>
2024-01-12 01:31:20 +0800

    新物品:奇異胞質聚合錠

---
+++ src/main/java/fabrictest/items/CellTurnedPolymerIngot.java
@@ -0,0 +1,11 @@
+package fabrictest.items;
+
+import net.minecraft.item.Item;
+
+public class CellTurnedPolymerIngot extends Item {
+
+    public CellTurnedPolymerIngot(Settings settings) {
+        super(settings);
+    }
+
+}
--- src/main/java/fabrictest/items/ModItems.java
+++ src/main/java/fabrictest/items/ModItems.java
@@ -7,8 +7,12 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item CELLS = new Cells(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item CELL_TURNED_POLYMER_INGOT = new CellTurnedPolymerIngot(
+            new Item.Settings().group(ItemGroup.MATERIALS));
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("fabrictest", "cells"), CELLS);
+        Registry.register(Registry.ITEM, new Identifier("fabrictest", "cell_turned_polymer_ingot"),
+                CELL_TURNED_POLYMER_INGOT);
     }
 }
