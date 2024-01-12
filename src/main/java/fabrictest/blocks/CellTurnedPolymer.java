09d83ce67fb1824533ace3b4af9361a9bf76e627
Texas0295 <Texas0295@outlook.com>
2024-01-13 00:50:54 +0800

    增加挖掘掉落及胞質聚合體的燒製配方

--- src/main/java/fabrictest/blocks/CellTurnedPolymer.java
+++ src/main/java/fabrictest/blocks/CellTurnedPolymer.java
@@ -1,9 +1,15 @@
 package fabrictest.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.world.explosion.Explosion;
 
 public class CellTurnedPolymer extends Block {
     public CellTurnedPolymer(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public boolean shouldDropItemsOnExplosion(Explosion explosion) {
+        return false;
     }
 }
